06779462bf18a7a9ca9fbf13bde483b79853740c
msai28258-glitch <msai28258@gmail.com>
2026-05-22 19:44:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openLogin() {
+
+    document.getElementById("popup").style.display = "flex";
+
+}
+
+function closeLogin() {
+
+    document.getElementById("popup").style.display = "none";
+
+}
